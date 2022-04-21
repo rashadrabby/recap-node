888a5b96e421548e70863ae7e5a26fb260c63567
rashadrabby <rashadulislamrabby@gmail.com>
2022-04-22 04:23:34 +0600

    steps added

---
+++ _steps.js
@@ -0,0 +1,27 @@
+/**
+ * Basic server setup
+ * ------------------------------------------
+ * 1. Create a folder.
+ * 2. Open command line to that folder.
+ * 3. npm init.
+ * 4. npm install express
+ * 5. Create index.js
+ * ---------------------------
+ * For express/node server
+ * ---------------------------
+ * 1. require express
+ * 2. create app variable
+ * 3. declare port
+ * 4. set app.get('/')
+ * 5. listen to port
+ * 6. node index.js or npm install -g nodemon
+ * 7. check browser for that port
+ * -----------------------------------------
+ * Get
+ * -----------------------------------------
+ * 1. cors middleware
+ * ------------------------------------------
+ * Post
+ * -----------------------------------------
+ * 1. 
+ */
